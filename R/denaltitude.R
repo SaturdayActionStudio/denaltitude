a4fe22b2d2104f2ac364aca7eea7934b5c75378d
Jordan Marshall <60512350+PlantEcology@users.noreply.github.com>
2023-08-17 13:50:17 -0400

    Add files via upload

---
+++ R/denaltitude.R
@@ -0,0 +1,40 @@
+#' Density Altitude (0.1)
+#'
+#' Simple function to calculate density altitude based on METAR data for a specific airport.
+#' @param airportCode Valid IATA airport code (character string).
+#' @param elevation Units for elevation displayed in "ft" (default) or "m" meters.
+#' @keywords elevation density altitude
+#' @export
+#' @examples
+#' denaltitude("FWA","ft")
+
+denaltitude=function(airportCode,elevation="ft"){
+  defaultW <- getOption("warn")
+  options(warn = -1)
+  airportDF <- as.data.frame(airportr::airport_detail(airportCode))
+  apAlt <- airportDF$Altitude
+  reMetar <- pmetar::metar_get(airport=airportCode)
+  presLoc <- 999
+  if (reMetar[1] == "No METAR found!") {reMetar[1]} else {
+    reMetar <- unlist(strsplit(reMetar," "))
+    for (j in 1:length(reMetar)) {
+      ifelse(substr(reMetar[j],1,1)=="A",ifelse(nchar(reMetar[j])==5,presLoc<-j,""),"")
+      if(presLoc == j){break}
+    }
+    presVec <- unlist(strsplit(reMetar[presLoc],"A"))
+    presHg <- strtoi(presVec[2])/100
+    for (i in 1:length(reMetar)) {
+      ifelse(grepl("/",reMetar[i])==TRUE,ifelse(nchar(reMetar[i])<=7,tempLoc<-i,""),"")
+    }
+    tempVec <- unlist(strsplit(reMetar[tempLoc],"/"))
+    tempC <- strtoi(tempVec[1])
+    presAlt<- (29.92-presHg)*1000+apAlt
+    denAlt <- presAlt+118.8*(tempC-13.39)
+    ifelse(elevation=="m",apAlt<-apAlt/3.281,"")
+    ifelse(elevation=="m",denAlt<-denAlt/3.281,"")
+    cat("City =",airportDF$City,":",airportDF$Country,"\n")
+    cat("Airport Altitude =",round(apAlt,0),elevation,"\n")
+    cat("Density Altitude =",round(denAlt,0),elevation)
+    options(warn = defaultW)
+  }
+}
